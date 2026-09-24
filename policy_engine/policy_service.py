e6401e1585234470d7d7d37380f3a0234b074bfc
MakamSriyasaswini <makamsriyasaswini@gmail.com>
2026-09-25 01:09:10 +0530

    Refactor Redis client initialization using from_url

--- policy_engine/policy_service.py
+++ policy_engine/policy_service.py
@@ -9,9 +9,7 @@ from gateway.config.settings import (
     DATABASE_NAME,
     DATABASE_USER,
     DATABASE_PASSWORD,
-    REDIS_URL,
-    REDIS_HOST,
-    REDIS_PORT
+    REDIS_URL
 )
 
 from policy_engine.policy import classify_object
@@ -23,11 +21,10 @@ if REDIS_URL:
         decode_responses=True
     )
 else:
-    redis_client = redis.Redis(
-        host=REDIS_HOST,
-        port=REDIS_PORT,
-        decode_responses=True
-    )
+redis_client = redis.from_url(
+    REDIS_URL,
+    decode_responses=True
+)
 
 
 def get_db_connection():
